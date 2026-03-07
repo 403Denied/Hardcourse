5b88f9df2e81776fe4694444c68469818e1bc65b
403denied <masonpetersen53@gmail.com>
2026-03-06 20:16:27 -0600

    Refactor unlink command; updated to allow staff to unlink any player from Discord and improved player validation

--- src/main/java/com/denied403/Hardcourse/Commands/Unlink.java
+++ src/main/java/com/denied403/Hardcourse/Commands/Unlink.java
@@ -1,12 +1,14 @@
 package com.denied403.Hardcourse.Commands;
 
 import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import net.dv8tion.jda.api.entities.UserSnowflake;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 
 import static com.denied403.Hardcourse.Discord.HardcourseDiscord.guild;
@@ -17,29 +19,32 @@ import static com.transfemme.dev.core403.Util.ColorUtil.Colorize;
 public class Unlink {
     public static LiteralCommandNode<CommandSourceStack> createCommand(String commandName){
         return Commands.literal(commandName)
-                .executes(ctx -> {
-                    CommandSender sender = ctx.getSource().getSender();
-                    if(!(sender instanceof Player player)) return 0;
-                    if(!checkpointDatabase.isLinked(player.getUniqueId())){
-                        player.sendMessage(Colorize("<prefix>You are not linked to a discord account."));
-                        return Command.SINGLE_SUCCESS;
-                    }
-                    if(!DiscordEnabled){
-                        player.sendMessage(Colorize("<prefix>Discord functionality is currently disabled. Please try again later."));
-                        return Command.SINGLE_SUCCESS;
-                    }
-                    if (guild != null) {
-                        if (linkedRole != null) {
-                            String discordId = checkpointDatabase.getDiscordId(player.getUniqueId());
-                            if (discordId != null) {
-                                guild.removeRoleFromMember(UserSnowflake.fromId(discordId), linkedRole).queue();
+                .requires(source -> source.getSender().hasPermission("hardcourse.staff"))
+                .then(Commands.argument("player", StringArgumentType.word())
+                        .suggests(WinnerTP::onlinePlayerSuggestions)
+                        .executes(ctx -> {
+                            OfflinePlayer target = Bukkit.getOfflinePlayer(StringArgumentType.getString(ctx, "player"));
+                            CommandSender sender = ctx.getSource().getSender();
+                            if(!target.hasPlayedBefore() && !target.isOnline()){
+                                ctx.getSource().getSender().sendMessage(Colorize("<prefix><error>Player not found!"));
+                                return 1;
                             }
-                        }
-                    }
-                    checkpointDatabase.unlinkDiscord(player.getUniqueId());
-                    linkManager.clearCode(player.getUniqueId());
-                    player.sendMessage(Colorize("<prefix>Your account has been <accent>unlinked<main> successfully."));
-                    return Command.SINGLE_SUCCESS;
-                }).build();
+                            if(!checkpointDatabase.isLinked(target.getUniqueId())){
+                                sender.sendMessage(Colorize("<prefix><error>Player is not linked!"));
+                                return Command.SINGLE_SUCCESS;
+                            }
+                            if (guild != null) {
+                                if (linkedRole != null) {
+                                    String discordId = checkpointDatabase.getDiscordId(target.getUniqueId());
+                                    if (discordId != null) {
+                                        guild.removeRoleFromMember(UserSnowflake.fromId(discordId), linkedRole).queue();
+                                    }
+                                }
+                            }
+                            checkpointDatabase.unlinkDiscord(target.getUniqueId());
+                            linkManager.clearCode(target.getUniqueId());
+                            ctx.getSource().getSender().sendMessage(Colorize("<prefix>Unlinked <accent>" + target.getName() + "'s <main>account from Discord."));
+                            return Command.SINGLE_SUCCESS;
+                    })).build();
     }
 }
